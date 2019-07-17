77fb2bc330aa346257abe625e2f72a28671e0d73
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-07-17 16:56:11 -0700

    Game Project (wip)

---
+++ src/Block.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Block {
+
+	
+	
+	
+	
+	Block(int x, int y, int width, int height){
+		
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	void draw(Graphics g) {
+	g.setColor(Color.BLACK);	
+	
+	
+	
+	
+	
+	
+	}
+	
+	void update() {
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/Objects.java
@@ -0,0 +1,41 @@
+import java.awt.Rectangle;
+
+public class Objects {
+
+	
+	int x;
+	int y;
+	int width;
+	int height;
+	int speed = 0;
+	boolean isActive = true;
+	Rectangle collisionBox;
+	
+	
+	Objects(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		collisionBox = new Rectangle(x,y,width,height);
+	}
+	
+	
+	void update(){
+		collisionBox.setBounds(x, y, width, height);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
